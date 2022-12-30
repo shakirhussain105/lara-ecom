32048187f79f8797115ee2ff3377dbfa1246db19
Shakir <shakir105@gmail.com>
2022-12-30 11:27:05 -0800

    Implementation of Basic Page Tempalte and Login form

---
+++ resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">LaraeCom</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Orders</a>
+                <a class="nav-link" href="#">Category</a>
+
+            </div>
+        </div>
+
+        <!-- <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> -->
+        <span class="navbar-text">
+            Cart
+        </span>
+    </div>
+</nav>
